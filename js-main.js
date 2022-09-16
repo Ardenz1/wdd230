a53d2c7f5442b752db8c87f7e28cfedbd81216de
Ardenz1 <ardenz@byui.edu>
2022-09-16 15:26:56 -0600

    updated 16 at 3

---
+++ js-main.js
@@ -0,0 +1,8 @@
+//Secquence(goes straight down ), selection (if statemnets ), repition (loop)
+let date = new Date(); 
+let year = date.getFullYear(); 
+
+document.querySelector('.year').innerHTML = year;  
+
+let currentdate = document.lastModified; 
+document.querySelector('.updated').innerHTML = currentdate;
