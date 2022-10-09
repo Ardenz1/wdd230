07c7fc07bad148e0b69615eaa63d6ff50dd76d18
Ardenz1 <ardenz@byui.edu>
2022-10-08 18:22:58 -0600

    chambershome1

---
+++ Chamber/js/js-chamber.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+   document.getElementById("primaryNav").classList.toggle("open");
+
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu; 
+
+
+//Current Date
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); 
+
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
